e586dedad7ba52c7891b029487c3967b4ab8a55d
Jeremias Jalil <jjejalil@gmail.com>
2021-05-08 14:27:48 -0300

    Tarea completada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -230,6 +230,7 @@ function tieneTresDigitos(numero){
 
 }
 
+
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.

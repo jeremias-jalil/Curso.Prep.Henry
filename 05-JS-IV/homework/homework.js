b291b5256a53542fccf66c5678fb8c219bc8eb66
Jeremias Jalil <jjejalil@gmail.com>
2021-05-08 12:48:21 -0300

    CP commiteado por jeremias.jalil a las 1620488900844

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -74,6 +74,7 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  return !!objeto[propiedad];
 }
 
 function verificarPassword(usuario, password) {
@@ -81,12 +82,18 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  return usuario.password===password;
 }
+//usuario = {
+//  password: 1234 
+// }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password=nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -94,6 +101,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -102,6 +111,11 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+for (let i=0; i<usuarios.length;i++){
+  usuarios[i].esPremium=true;
+}
+return usuarios;
+
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -111,6 +125,13 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+let suma=0;
+for (i=0;i<usuario.posts.length;i++){
+  suma=suma+usuario.posts[i].likes;
+}
+return suma;
+
+
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -123,6 +144,12 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  function calcularPrecioDescuento(){
+    let descuento=this.precio-(this.precio*this.porcentajeDeDescuento);
+    return descuento;
+  }
+  producto.calcularPrecioDescuento=calcularPrecioDescuento;
+  return producto;
 
 }
 
